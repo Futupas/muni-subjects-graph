d14c10f3fbe3c9669f45073c2c538e1a1a1d3d10
Alex Panov <futupas@gmail.com>
2024-02-07 14:32:38 +0100

    add: table and block parsing

--- parser.js
+++ parser.js
@@ -8,5 +8,42 @@
         .filter(x => (x.tagName === 'P' || x.tagName === 'TABLE' || x.tagName === 'FONT') && x.innerText);
 
     console.log(elements);
+
+    
+    const blocks = [];
+
+    for (let i = 0; i < elements.length; i++) {
+        if (elements[i].tagName !== 'TABLE') continue;
+        
+        const prevElement = i === 0 ? null : elements[i-1];
+        
+        const blockName = prevElement?.tagName !== 'TABLE' ? prevElement.innerText : null;
+
+        const table = [];
+        
+
+        const trs = Array.from(elements[i].querySelectorAll('tr'));
+        const columnNames = Array.from(trs[0].querySelectorAll('td')).map(x => x.innerText);
+
+        for (let rowI = 1; rowI < trs.length; rowI++) {
+            const row = {};
+            const cols = Array.from(trs[rowI].querySelectorAll('td')).map(x => x.innerText);
+            for (let colI = 1; colI < columnNames.length; colI++) {
+                row[columnNames[colI]] = cols[colI];
+            }
+            table.push(row);
+        }
+
+        blocks.push({
+            name: blockName,
+            tableEl: elements[i],
+            prevEl: prevElement,
+            table
+        });
+        
+    }
+
+    console.log(blocks);
+
 }
 
