b2f6367761afc52fff4aee14229b404e7931eee4
xLamiacea <159273192+xLamiacea@users.noreply.github.com>
2024-02-18 20:25:49 +0100

    add: faculty dict

--- parser.py
+++ parser.py
@@ -13,11 +13,25 @@ file_path = 'C:/Users/user/OneDrive/Документи/GitHub/muni-subjects-grap
 with open(file_path, 'r', encoding='utf-8') as file:
     original_data = json.load(file)
 
-# store transformed data
+# Dict for faculties used in generated link
+faculty_links = {
+    "FI": "fi", 
+    "PrF": "law",
+    "FSS": "fss",
+    "LF": "med",
+    "PřF": "sci",
+    "FF": "phil",
+    "PdF": "ped",
+    "FaF": "pharm",
+    "ESF": "econ",
+    "FSpS": "fsps"
+}
+
 transformed_data = []
 
 for item in original_data:
     for subject in item["table"]:
+        
         # Extract faculty abbreviation if code contains it, otherwise set faculty to "FI"
         if ":" in subject["kód"]:
             faculty, code = subject["kód"].split(":", 1)
@@ -25,14 +39,17 @@ for item in original_data:
             faculty = "FI"
             code = subject["kód"]
 
+        # Get the corresponding link for the faculty
+        link_faculty = faculty_links.get(faculty)
+
         transformed_subject = {
             "faculty": faculty,
             "code": code,
             "name": subject["název"],
             "conclusion": subject["ukon."],
-            "credits": int(subject["kred."].split()[0]),  
+            "credits": int(subject["kred."].split()[0]),
             "prerequisites": subject["prerekvizity"],
-            "link": f"https://is.muni.cz/predmet/{faculty.lower()}/{code}", # need to fix so it'll work for all faculties
+            "link": f"https://is.muni.cz/predmet/{link_faculty}/{code}",
             "additionalData": {
                 "rozsah": subject["rozsah"],
                 "garant": subject["garant"],
